f863120b33f49baf326ed3ab5294a60a3c96e371
arcsine <tangscn122@gmail.com>
2020-08-21 20:58:10 +0800

    Initial commit

---
+++ app/src/main/java/com/onenote/twonote/MainActivity.java
@@ -0,0 +1,14 @@
+package com.onenote.twonote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
