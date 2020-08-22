6d720611b0f92c66ec809522baecff3a2eab4ca1
owl10124 <shevonnechia1@gmail.com>
2020-08-22 13:05:41 +0800

    here you go rain

--- app/src/main/java/com/onenote/twonote/FocusFragment.java
+++ app/src/main/java/com/onenote/twonote/FocusFragment.java
@@ -4,8 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Date;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class FocusFragment extends Fragment {
@@ -14,4 +18,21 @@ public class FocusFragment extends Fragment {
             View v = inflater.inflate(R.layout.fragment_focus, container, false);
             return v;
         }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        TextView timerView = getActivity().findViewById(R.id.timerView);
+        TextView eventView = getActivity().findViewById(R.id.eventView); //event name
+        TextView subjView = getActivity().findViewById(R.id.subjView); //subject name
+        Event e = Event.correlate(new Date());
+        Date deadline = e.getEndDate();
+
+        eventView.setText(e.getName());
+        subjView.setText(e.getTopic().getName());
+
+        //your code here
+
+        // timerView.setText(...);
+    }
 }
